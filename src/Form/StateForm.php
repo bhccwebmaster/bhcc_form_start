ef08770c1eddaf0926055f09b618d5172df99b83
Andy Broomfield <andy.broomfield@brighton-hove.gov.uk>
2023-01-25 16:32:28 +0000

    Add form groups to the state form
    Add options to disable the state of each form group to the state form.

--- src/Form/StateForm.php
+++ src/Form/StateForm.php
@@ -35,6 +35,13 @@ class StateForm extends FormBase {
   protected $cacheTagsInvalidator;
 
   /**
+   * Entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
@@ -42,6 +49,7 @@ class StateForm extends FormBase {
     $instance->state = $container->get('state');
     $instance->configFactory = $container->get('config.factory');
     $instance->cacheTagsInvalidator = $container->get('cache_tags.invalidator');
+    $instance->entityTypeManager = $container->get('entity_type.manager');
     return $instance;
   }
 
@@ -56,17 +64,44 @@ class StateForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+
+    // Global form start status.
     $form['on_off'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Form start state'),
     ];
     $form['on_off']['forms_status'] = [
       '#type' => 'radios',
-      '#title' => $this->t('Forms Status'),
+      '#title' => $this->t('All forms status'),
       '#options' => [1 => $this->t('On'), 0 => $this->t('Off')],
       '#default_value' => $this->state->get(self::STATE_PREFIX . 'forms_status') ?? 1,
       '#weight' => '0',
     ];
+
+    // Get form start groups.
+    $form_group_storage = $this->entityTypeManager
+      ->getStorage('bhcc_form_start_group');
+    $form_start_group_ids = $form_group_storage->getQuery()
+      ->execute();
+    $form_start_groups = array_map(function ($id) use ($form_group_storage) {
+      return $form_group_storage->load($id)->label();
+    }, $form_start_group_ids);
+
+    $form['on_off']['form_groups_status'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Form start groups'),
+      '#weight' => '10',
+    ];
+    foreach ($form_start_groups as $group_key => $group_value) {
+      $form['on_off']['form_groups_status']['forms_status__' . $group_key] = [
+        '#type' => 'radios',
+        '#title' => $this->t('%group_name status', ['%group_name' => $group_value]),
+        '#options' => [1 => $this->t('On'), 0 => $this->t('Off')],
+        '#default_value' => $this->state->get(self::STATE_PREFIX . 'forms_status__' . $group_key) ?? 1,
+      ];
+    }
+
+    // Forms off message.
     $default_message = $this->state->get(self::STATE_PREFIX . 'message_to_display_when_form_off');
     $form['on_off']['message_to_display_when_form_off'] = [
       '#type' => 'text_format',
@@ -74,7 +109,7 @@ class StateForm extends FormBase {
       '#rows' => 3,
       '#default_value' => $default_message['value'] ?? 'This form is currently unavailable.',
       '#format' => $default_message['format'] ?? NULL,
-      '#weight' => '0',
+      '#weight' => '20',
     ];
     $form['submit'] = [
       '#type' => 'submit',
