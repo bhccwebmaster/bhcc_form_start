2995dfb0faaf40b21fc778fa6c7acc0c567fefc7
Hannah Harvey <hannah.harvey@brighton-hove.gov.uk>
2023-02-23 14:02:04 +0000

    Fix workplace form start pages not working.
    Resolved by changing the line 187
    from 'workplace.brighton-hove.gov.uk/form/', to 'workplace.brighton-hove.gov.uk',
    
    two form groups =  Admin / Parking
    
    Tested with three forms
    parking form
    http://workplace.bhcclocalgov.test/apply-professional-carer-parking-permit-internal
    
    Internal different forms
    http://workplace.bhcclocalgov.test/pcc-register-upload
    http://workplace.bhcclocalgov.test/request-access-citizen-viewer-0
    
    Steps to recreate error
    Change line 187 back to 'workplace.brighton-hove.gov.uk/form/',
    and set a form to the parking group nothing happens
    
    Change link 187 to 'workplace.brighton-hove.gov.uk', form will
    have standard message
    This form is down for maintenance, please try again later.

--- src/Plugin/Field/FieldFormatter/FormStartFieldFormatter.php
+++ src/Plugin/Field/FieldFormatter/FormStartFieldFormatter.php
@@ -184,7 +184,7 @@ class FormStartFieldFormatter extends LinkFormatter {
         'formsstg.brighton-hove.gov.uk',
         'formsdev.brighton-hove.gov.uk',
         'citizenform.brighton-hove.gov.uk',
-        'workplace.brighton-hove.gov.uk/form/',
+        'workplace.brighton-hove.gov.uk',
       ];
 
       // Check if each form needs to be disabled.
